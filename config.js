6f2a7b36df1428a0f71b0e1a2f87789d1203a14f
David Renshaw <dwrenshaw@gmail.com>
2014-08-21 13:07:30 -0400

    config.js for plain operation

---
+++ config.js
@@ -0,0 +1,27 @@
+{
+    "host": "192.168.0.50",
+    "port": 16242,
+    "dbPath": "groovebasin.db",
+    "musicDirectory": "/home/dwrensha/music",
+    "permissions": {
+        "ElmgP7Ms6Pkw": {
+            "admin": true,
+            "read": true,
+            "add": true,
+            "control": true
+        }
+    },
+    "defaultPermissions": {
+        "read": true,
+        "add": true,
+        "control": true
+    },
+    "lastFmApiKey": "bb9b81026cd44fd086fa5533420ac9b4",
+    "lastFmApiSecret": "2309a40ae3e271de966bf320498a8f09",
+    "mpdHost": "0.0.0.0",
+    "mpdPort": 6600,
+    "acoustidAppKey": "bgFvC4vW",
+    "encodeQueueDuration": 8,
+    "sslKey": false,
+    "sslCert": false
+}
